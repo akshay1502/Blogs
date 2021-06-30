5beec350018210b86d9679c82865367c6796ee8d
akshay shinde <akshayshinde7289@gmail.com>
2021-06-30 13:16:56 +0530

    show likes in realtime in progress

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,18 @@
 // import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useState } from "react";
 
 const BlogDetails = () => {
+    const [likes, setLikes] = useState('');
+
     const { id } = useParams();
     const { data, isPending, error } = useFetch('http://localhost:8000/blogs/'+id);
-    // console.log(data);
+    console.log(data);
+    if(data){
+        // setLikes(data.likes);
+        console.log(`it's here`);
+    }
     const history = useHistory();
     const handleclick = () => {
         fetch('http://localhost:8000/blogs/'+id,{
@@ -15,21 +22,22 @@ const BlogDetails = () => {
     }
 
     const handlelike = () => {
-        // data.likes += 1;
-        const doc = { ...data };
-        console.log(doc);
-        fetch('http://localhost:8000/blogs',{
-            method: 'POST',
+        // const doc = { ...data, likes: data.likes+1, name: 'unknown error' };
+        const doc = {
+            likes: data.likes+1
+        };
+        // console.log(doc);
+        fetch('http://localhost:8000/blogs/'+id,{
+            method: 'PATCH',
             body: JSON.stringify(doc),
             headers: {'Content-Type' : 'application/json'}
         })
+        .then(res => res.json())
+        .then(res => setLikes(res.likes))
         .then(() => {
-            console.log(`blog added`);
-            history.push('/'); 
+            console.log(`new blog likes added`);
+            
         });
-        // axios.post('http://localhost:8000/blogs',doc)
-        //     .then(() => { console.log(`done`); history.push('/') });
-
     }
 
     return ( 
@@ -41,7 +49,7 @@ const BlogDetails = () => {
                     <h2>{ data.title }</h2>
                     <p>Written by { data.author }</p>
                     <div>{data.body}</div>
-                    <button onClick={handlelike}>Likes {data.likes}</button>
+                    <button onClick={handlelike}>Likes {likes}</button>
                     <button onClick={handleclick}>Delete</button>
                 </article>
             }
